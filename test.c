8924f5c894b8ffb1434d950aebd0b279927509d4
XadillaX <admin@xcoder.in>
2014-12-09 11:59:28 +0800

    first commit for test.c

---
+++ test.c
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.c
+ *
+ *    Description:  Testing code for xmempool.
+ *
+ *        Version:  1.0
+ *        Created:  2014/12/09 11时53分52秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  XadillaX
+ *   Organization:  Huaban.com
+ *
+ * =====================================================================================
+ */
+#include "xmempool.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    return 0;
+}
+
