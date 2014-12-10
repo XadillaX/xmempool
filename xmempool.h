b06351836c9b51952a3d98c438df6626dda8738c
XadillaX <admin@xcoder.in>
2014-12-10 14:44:43 +0800

    add c++ support

--- xmempool.h
+++ xmempool.h
@@ -18,6 +18,10 @@
 #ifndef __XMEMPOOL_H__
 #define __XMEMPOOL_H__
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef void*               xmem_pool_handle;
 
 // 5 APIs so far
@@ -26,6 +30,10 @@ extern void                 xmem_destroy_pool(xmem_pool_handle pool);
 extern void*                xmem_alloc(xmem_pool_handle handle);
 extern int                  xmem_free(xmem_pool_handle handle, void* pointer);
 extern void                 xmem_print_info(xmem_pool_handle pool);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
 
