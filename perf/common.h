6b6274bc06c3cbb3ff362aa4e563eaa8b144162c
XadillaX <admin@xcoder.in>
2014-12-16 14:42:14 +0800

    update rdtsc.h

--- perf/common.h
+++ perf/common.h
@@ -18,24 +18,9 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include "rdtsc.h"
 
 #define TEST_COUNT 1000000
-
-static inline uint64_t rdtsc(void)
-{
-    union {
-        uint64_t tsc_64;
-        struct {
-            uint32_t lo_32;
-            uint32_t hi_32;
-        };
-    } tsc;
- 
-    __asm volatile("rdtsc" :
-        "=a" (tsc.lo_32),
-        "=d" (tsc.hi_32));
-    return tsc.tsc_64;
-}
 
 typedef struct test_struct {
     int id;
